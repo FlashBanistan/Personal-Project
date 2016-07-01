55cbd15b92097e15bc069aac8b89ae37629ec0bd
Aaron Chesley <FlashBanistan66@gmail.com>
2016-07-01 07:09:11 -0600

    Deleted

--- public/js/controllers/setsCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('FlashCards').controller('setsCtrl', function($scope, flashcard, sets){
-
-  $scope.sets = sets;
-  console.log($scope.sets)
-});
--- public/js/directives/menu-bar.js
+++
@@ -1,6 +0,0 @@
-angular.module('FlashCards').directive('menuBar', function(){
-  return {
-    restrict: "EA",
-    templateUrl: './js/directives/directive-templates/menu-bar.html'
-  }
-});
